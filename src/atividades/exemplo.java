9301686fb456d1508586b3e27c4fbe0fc9e41cd5
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2018-03-27 23:28:47 -0300

    Adicionando exemplo

--- src/atividades/exemplo.java
+++ src/atividades/exemplo.java
@@ -5,30 +5,44 @@ import java.util.ArrayList;
 public class exemplo {
 	private static ArrayList<Aluno> alunos = new ArrayList<Aluno>();
 	public static void main(String[] args) {
+		cadastrarAluno("Maria", "1");
+		cadastrarAluno("Amadeu", "3");
+		System.out.println("Maria e Amadeu cadastrados com sucesso");
+		System.out.println("Total de alunos cadastrados:" + quantidadeDeAlunosCadastrados());		
+		removerAluno("1"); // removendo Maria
+		System.out.println("Total de alunos cadastrados:" + quantidadeDeAlunosCadastrados());
 		
 	}
 	
 	// Para isso funcionar você deve criar a classe Aluno no mesmo pacote que essa classe.
-	private Aluno criaAluno(String nome, String matricula){
+	private static Aluno criaAluno(String nome, String matricula){
 		Aluno aluno = new Aluno(nome, matricula);
 		return aluno;
 	}
 	
-	// Dica: use o método criaAluno!
-	private void cadastrarAluno(String nome, String matricula){
+	// IMPLEMENTE ESSE MÉTODO. Dica: use o método criaAluno! DUAS linhas resolve isso!
+	private static void cadastrarAluno(String nome, String matricula){
 		
 	}
 	
-	private void removerAluno(String matricula){
+	// IMPLEMENTE ESSE MÉTODO. Dica: use um for e um if!
+	private static void removerAluno(String matricula){
 		
 	}
 	
-	private boolean verificaSeOAlunoEstaCadastrado(String matricula){
+	// IMPLEMENTE ESSE MÉTODO. Dica: use um for e um if!
+	private static boolean verificaSeOAlunoEstaCadastrado(String matricula){
 		
 	}
 	
+	// IMPLEMENTE ESSE MÉTODO. Dica: use um for e um if!
 	private Aluno recuperarAluno(String matricula){
 		
 	}
+	
+	// IMPLEMENTE ESSE MÉTODO. UMA linha resolve isso!
+	private int quantidadeDeAlunosCadastrados(){
+			
+		}
 
 }
