f1acc63bbb3d6b6aba8e4530454caccc5604e58f
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2018-03-27 23:31:32 -0300

    Melhorando classe exemplo

--- src/atividades/exemplo.java
+++ src/atividades/exemplo.java
@@ -12,37 +12,37 @@ public class exemplo {
 		removerAluno("1"); // removendo Maria
 		System.out.println("Total de alunos cadastrados:" + quantidadeDeAlunosCadastrados());
 		
-		}
+	}
 	
 	// Para isso funcionar você deve criar a classe Aluno no mesmo pacote que essa classe.
 	private static Aluno criaAluno(String nome, String matricula){
 		Aluno aluno = new Aluno(nome, matricula);
 		return aluno;
-		}
+	}
 	
 	// IMPLEMENTE ESSE MÉTODO. Dica: use o método criaAluno! DUAS linhas resolve isso!
 	private static void cadastrarAluno(String nome, String matricula){
 		
-		}
+	}
 	
 	// IMPLEMENTE ESSE MÉTODO. Dica: use um for e um if!
 	private static void removerAluno(String matricula){
 		
-		}
+	}
 	
 	// IMPLEMENTE ESSE MÉTODO. Dica: use um for e um if!
 	private static boolean verificaSeOAlunoEstaCadastrado(String matricula){
 		
-		}
+	}
 	
 	// IMPLEMENTE ESSE MÉTODO. Dica: use um for e um if!
 	private Aluno recuperarAluno(String matricula){
 		
-		}
+	}
 	
 	// IMPLEMENTE ESSE MÉTODO. UMA linha resolve isso!
 	private int quantidadeDeAlunosCadastrados(){
 			
-		}
+	}
 
 }
